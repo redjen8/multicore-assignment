62ed102950d29222045cb1576a5f6785fb1a8951
redjen8 <seokwoo2525@gmail.com>
2022-05-09 16:51:56 +0900

    20220509 jsw Feat : ArrayBlockingQueue 사용하여 1차 구현

--- proj2/prob1/ParkingBlockingQueue.java
+++ proj2/prob1/ParkingBlockingQueue.java
@@ -1,49 +1,50 @@
 package proj2.prob1;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 class ParkingGarage {
-    private int places;
-    public ParkingGarage(int places) {
-      if (places < 0)
-        places = 0;
-      this.places = places;
+    private BlockingQueue<String> places;
+    public ParkingGarage(int capacity) {
+    	if (capacity < 0)
+    		capacity = 0;
+    	this.places = new ArrayBlockingQueue<>(capacity);
     }
-    public synchronized void enter() { // enter parking garage
-      while (places == 0) {
+    public synchronized void enter(String carName) { // enter parking garage
         try {
-          wait();
-        } catch (InterruptedException e) {}
-      }
-      places--;
+        	places.put(carName);
+        } catch (InterruptedException e) {
+			e.printStackTrace();
+		}
     }
-    public synchronized void leave() { // leave parking garage
-      places++;
-      notify();
+    public synchronized String leave() { // leave parking garage
+		String carName = "";
+		try {
+			carName = places.take();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return carName;
     }
-    public synchronized int getPlaces()
-    {
-      return places;
-    }
-  }
+}
   
-  
-  class Car extends Thread {
-    private ParkingGarage parkingGarage;
-    public Car(String name, ParkingGarage p) {
-      super(name);
-      this.parkingGarage = p;
-      start();
-    }
-  
-    private void tryingEnter()
-    {
-        System.out.println(getName()+": trying to enter");
-    }
-  
-  
+class Car extends Thread {
+	private ParkingGarage parkingGarage;
+
+	public Car(String name, ParkingGarage p) {
+		super(name);
+		this.parkingGarage = p;
+		start();
+	}
+
+	private void tryingEnter()
+	{
+		System.out.println(getName()+": trying to enter");
+	}
+
     private void justEntered()
     {
         System.out.println(getName()+": just entered");
-  
     }
   
     private void aboutToLeave()
@@ -57,31 +58,38 @@ class ParkingGarage {
     }
   
     public void run() {
-      while (true) {
-        try {
-          sleep((int)(Math.random() * 10000)); // drive before parking
-        } catch (InterruptedException e) {}
-        tryingEnter();
-        parkingGarage.enter();
-        justEntered();
-        try {
-          sleep((int)(Math.random() * 20000)); // stay within the parking garage
-        } catch (InterruptedException e) {}
-        aboutToLeave();
-        parkingGarage.leave();
-        Left();
-  
-      }
+    	while (true) {
+        	try {
+        		sleep((int)(Math.random() * 10000)); // drive before parking
+        	} catch (InterruptedException e) {}
+			
+			tryingEnter();
+			try {
+				parkingGarage.enter(getName());
+			}
+			catch (NumberFormatException e) {
+				e.printStackTrace();
+			}
+			justEntered();
+
+			try {
+				sleep((int)(Math.random() * 20000)); // stay within the parking garage
+			} catch (InterruptedException e) {}
+			
+			aboutToLeave();
+			System.out.println(parkingGarage.leave());
+			Left();
+      	}
     }
   }
   
   
-  public class ParkingBlockingQueue {
+public class ParkingBlockingQueue {
     public static void main(String[] args){
-      ParkingGarage parkingGarage = new ParkingGarage(7);
-      for (int i=1; i<= 10; i++) {
-        Car c = new Car("Car "+i, parkingGarage);
-      }
+      	ParkingGarage parkingGarage = new ParkingGarage(7);
+      	for (int i=1; i<= 10; i++) {
+        	Car c = new Car("Car "+i, parkingGarage);
+      	}
     }
-  }
+}
   
