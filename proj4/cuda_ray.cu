fdecb88326a37401fe0d762a096769218261bdc2
redjen8 <seokwoo2525@gmail.com>
2022-06-06 16:28:14 +0900

    20220606 jsw Feat : cuda 레이트레이싱 예제 최초 추가

--- proj4/cuda_ray.cu
+++ proj4/cuda_ray.cu
@@ -1,7 +1,60 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <cuda.h>
 
-__global__ void add(int *a, int *b, int *c) {
-    *c = *a + *b;
+#define CUDA 0
+#define OPENMP 1
+#define SPHERES 20
+
+#define rnd( x ) (x * rand() / RAND_MAX)
+#define INF 2e10f
+#define DIM 2048
+
+struct Sphere {
+    float   r,b,g;
+    float   radius;
+    float   x,y,z;
+    float hit( float ox, float oy, float *n ) {
+        float dx = ox - x;
+        float dy = oy - y;
+        if (dx*dx + dy*dy < radius*radius) {
+            float dz = sqrtf( radius*radius - dx*dx - dy*dy );
+            *n = dz / sqrtf( radius * radius );
+            return dz + z;
+        }
+        return -INF;
+    }
+};
+
+__global__ void kernel_compute(int x, int y, Sphere* s, unsigned char* ptr)
+{
+	int offset = x + y*DIM;
+	float ox = (x - DIM/2);
+	float oy = (y - DIM/2);
+
+	//printf("x:%d, y:%d, ox:%f, oy:%f\n",x,y,ox,oy);
+
+	float r=0, g=0, b=0;
+	float   maxz = -INF;
+	for(int i=0; i<SPHERES; i++) {
+		float   n;
+		float   t = s[i].hit( ox, oy, &n );
+		if (t > maxz) {
+			float fscale = n;
+			r = s[i].r * fscale;
+			g = s[i].g * fscale;
+			b = s[i].b * fscale;
+			maxz = t;
+		} 
+	}
+
+	ptr[offset*4 + 0] = (int)(r * 255);
+	ptr[offset*4 + 1] = (int)(g * 255);
+	ptr[offset*4 + 2] = (int)(b * 255);
+	ptr[offset*4 + 3] = 255;
 }
 
 void ppm_write(unsigned char* bitmap, int xdim,int ydim, FILE* fp)
@@ -20,27 +73,43 @@ void ppm_write(unsigned char* bitmap, int xdim,int ydim, FILE* fp)
 }
 
 int main (void) {
-    int a, b, c;
-    int *d_a, *d_b, *d_c;
-    int size = sizeof(int);
+    int no_threads;
+	int option;
+	int x,y;
+	unsigned char* bitmap;
 
-    cudaMalloc((void **) &d_a, size);
-    cudaMalloc((void **) &d_b, size);
-    cudaMalloc((void **) &d_c, size);
-    
-    a = 2;
-    b = 7;
+	srand(time(NULL));
 
-    cudaMemcpy(d_a, &a, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, &b, size, cudaMemcpyHostToDevice);
+	FILE* fp = fopen("result.ppm","w");
 
-    add<<<1, 1>>> (d_a, d_b, d_c);
+    clock_t begin = clock();
+	Sphere *temp_s = (Sphere*)malloc( sizeof(Sphere) * SPHERES );
+	for (int i=0; i<SPHERES; i++) {
+		temp_s[i].r = rnd( 1.0f );
+		temp_s[i].g = rnd( 1.0f );
+		temp_s[i].b = rnd( 1.0f );
+		temp_s[i].x = rnd( 2000.0f ) - 1000;
+		temp_s[i].y = rnd( 2000.0f ) - 1000;
+		temp_s[i].z = rnd( 2000.0f ) - 1000;
+		temp_s[i].radius = rnd( 200.0f ) + 40;
+	}
+	
+	bitmap=(unsigned char*)malloc(sizeof(unsigned char)*DIM*DIM*4);
+	for (x=0;x<DIM;x++) 
+	{	
+		for (y=0;y<DIM;y++) 
+		{
+			kernel_compute(x,y,temp_s,bitmap);
+		}
+	}
+	ppm_write(bitmap,DIM,DIM,fp);
+    clock_t end = clock();
+    double timeDiff = (double) (end-begin) / CLOCKS_PER_SEC;
+	printf("Execution time : %lfms\n", timeDiff * 1000);
+	printf("[%s] was generated.\n", "result.ppm");
+	fclose(fp);
+	free(bitmap);
+	free(temp_s);
 
-    cudaMemcpy(&c, d_c, size, cudaMemcpyDeviceToHost);
-    printf("c : %d\n", c);
-
-    cudaFree(d_a);
-    cudaFree(d_b);
-    cudaFree(d_c);
-    return 0;
+	return 0;
 }
