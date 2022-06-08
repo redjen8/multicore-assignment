e999fc1a7da63290c2e91882d095737d00fe3f22
redjen8 <seokwoo2525@gmail.com>
2022-06-08 16:21:19 +0900

    20220608 jsw Feat : cuda_ray 작성 완료, 정상 동작 확인

--- proj4/cuda_ray.cu
+++ proj4/cuda_ray.cu
@@ -5,19 +5,27 @@
 #include <math.h>
 #include <cuda.h>
 
-#define CUDA 0
-#define OPENMP 1
 #define SPHERES 20
 
 #define rnd( x ) (x * rand() / RAND_MAX)
 #define INF 2e10f
 #define DIM 2048
 
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess)
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
+
 struct Sphere {
     float   r,b,g;
     float   radius;
     float   x,y,z;
-    float hit( float ox, float oy, float *n ) {
+    __device__ float hit( float ox, float oy, float *n ) {
         float dx = ox - x;
         float dy = oy - y;
         if (dx*dx + dy*dy < radius*radius) {
@@ -29,19 +37,21 @@ struct Sphere {
     }
 };
 
-__global__ void kernel_compute(int x, int y, Sphere* s, unsigned char* ptr)
+
+__global__ void kernel_compute(Sphere* s, unsigned char* ptr)
 {
-	int offset = x + y*DIM;
-	float ox = (x - DIM/2);
-	float oy = (y - DIM/2);
-
-	//printf("x:%d, y:%d, ox:%f, oy:%f\n",x,y,ox,oy);
-
+	int tx = threadIdx.x + blockIdx.x * blockDim.x;
+	int ty = threadIdx.y + blockIdx.y * blockDim.y;
+	int offset = tx + ty*DIM;
+	float ox = (tx - DIM/2);
+	float oy = (ty - DIM/2);
+	// printf("x:%d, y:%d, ox:%f, oy:%f\n",tx,ty,ox,oy);
 	float r=0, g=0, b=0;
-	float   maxz = -INF;
+	float maxz = -INF;
 	for(int i=0; i<SPHERES; i++) {
-		float   n;
+		float n;
 		float   t = s[i].hit( ox, oy, &n );
+
 		if (t > maxz) {
 			float fscale = n;
 			r = s[i].r * fscale;
@@ -73,43 +83,53 @@ void ppm_write(unsigned char* bitmap, int xdim,int ydim, FILE* fp)
 }
 
 int main (void) {
-    int no_threads;
-	int option;
-	int x,y;
-	unsigned char* bitmap;
+	unsigned char* bitmap_h;
 
 	srand(time(NULL));
 
-	FILE* fp = fopen("result.ppm","w");
+	FILE* fp = fopen("result_cuda.ppm","w");
 
     clock_t begin = clock();
-	Sphere *temp_s = (Sphere*)malloc( sizeof(Sphere) * SPHERES );
+    // allocate memory for host
+	Sphere *sphere_h = (Sphere*)malloc( sizeof(Sphere) * SPHERES );
 	for (int i=0; i<SPHERES; i++) {
-		temp_s[i].r = rnd( 1.0f );
-		temp_s[i].g = rnd( 1.0f );
-		temp_s[i].b = rnd( 1.0f );
-		temp_s[i].x = rnd( 2000.0f ) - 1000;
-		temp_s[i].y = rnd( 2000.0f ) - 1000;
-		temp_s[i].z = rnd( 2000.0f ) - 1000;
-		temp_s[i].radius = rnd( 200.0f ) + 40;
+		sphere_h[i].r = rnd( 1.0f );
+		sphere_h[i].g = rnd( 1.0f );
+		sphere_h[i].b = rnd( 1.0f );
+		sphere_h[i].x = rnd( 2000.0f ) - 1000;
+		sphere_h[i].y = rnd( 2000.0f ) - 1000;
+		sphere_h[i].z = rnd( 2000.0f ) - 1000;
+		sphere_h[i].radius = rnd( 200.0f ) + 40;
 	}
-	
-	bitmap=(unsigned char*)malloc(sizeof(unsigned char)*DIM*DIM*4);
-	for (x=0;x<DIM;x++) 
-	{	
-		for (y=0;y<DIM;y++) 
-		{
-			kernel_compute(x,y,temp_s,bitmap);
-		}
-	}
-	ppm_write(bitmap,DIM,DIM,fp);
+	bitmap_h=(unsigned char*)malloc(sizeof(unsigned char)*DIM*DIM*4);
+
+    //allocate memory for device
+    int sphereSize = sizeof(Sphere) * SPHERES;
+    int bitmapSize = sizeof(unsigned char) * DIM * DIM * 4;
+    Sphere* sphere_d = (Sphere*) malloc (sphereSize);
+    unsigned char* bitmap_d = (unsigned char*) malloc (bitmapSize);
+    gpuErrchk(cudaMalloc((void**)&sphere_d, sphereSize));
+    gpuErrchk(cudaMalloc((void**)&bitmap_d, bitmapSize));
+
+    gpuErrchk(cudaMemcpy(sphere_d, sphere_h, sphereSize, cudaMemcpyHostToDevice));
+    gpuErrchk(cudaMemcpy(bitmap_d, bitmap_h, bitmapSize, cudaMemcpyHostToDevice));
+	dim3 dimBlock(32, 32);
+	dim3 dimGrid(64, 64);
+    kernel_compute<<<dimGrid, dimBlock>>>(sphere_d, bitmap_d);
+	gpuErrchk(cudaDeviceSynchronize());
+
+	gpuErrchk(cudaMemcpy(bitmap_h, bitmap_d, bitmapSize, cudaMemcpyDeviceToHost));
+
+	gpuErrchk(cudaFree(sphere_d));
+	gpuErrchk(cudaFree(bitmap_d));
+
+	ppm_write(bitmap_h,DIM,DIM,fp);
     clock_t end = clock();
     double timeDiff = (double) (end-begin) / CLOCKS_PER_SEC;
 	printf("Execution time : %lfms\n", timeDiff * 1000);
-	printf("[%s] was generated.\n", "result.ppm");
+	printf("[%s] was generated.\n", "result_cuda.ppm");
 	fclose(fp);
-	free(bitmap);
-	free(temp_s);
-
+	free(bitmap_h);
+	free(sphere_h);
 	return 0;
 }
