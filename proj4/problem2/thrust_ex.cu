d1d233c8e1cd63f73fc93f02a9fa809b0e10eb72
redjen8 <seokwoo2525@gmail.com>
2022-06-08 23:34:41 +0900

    20220608 jsw Feat : prob2 thrust 사용하여 PI값 계산

--- proj4/problem2/thrust_ex.cu
+++ proj4/problem2/thrust_ex.cu
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cuda.h>
 #include <cmath>
+#include <thrust/fill.h>
+#include <thrust/device_vector.h>
 
 #define N 1000000000
 
@@ -14,19 +16,28 @@ inline void gpuAssert(cudaError_t code, char *file, int line, bool abort=true)
    }
 }
 
-double step;
-
-__global__ void kernel_compute() {
-    printf("Hello, world!\n");
-}
-
-__shared__ int sum;
+struct convert_pi
+{
+	const double step = 0.000000001;
+	__device__ 
+	double operator() (const int& x) const {
+		return 4.0 / (1.0 + ((x + 0.5) * step) * ((x + 0.5) * step));
+	}
+};
 
 int main(void) {
-    step = 1.0 / (double)N;
-    dim3 dimBlock(32, 32);
-    dim3 dimGrid(1024, 1024);
-    kernel_compute<<<dimGrid, dimBlock>>>();
-    gpuErrchk(cudaDeviceSynchronize());
-    return 0;
+	double alpha = 1 / (double) N;
+	thrust::device_vector<int> data(N);
+	thrust::sequence(data.begin(), data.end());
+	// for (int i = 0; i<data.size(); i++) {
+	// 	std::cout << "data [" << i << "] = " << data[i] << std::endl;
+	// }
+	convert_pi unary_op;
+	thrust::plus<double> binary_op;
+	double result = thrust::transform_reduce(thrust::device, data.begin(), data.end(), unary_op, (double) 0.0, binary_op) * alpha;
+	gpuErrchk(cudaDeviceSynchronize());
+	// std::cout << alpha << std::endl;
+	std::cout.precision(24);
+	std::cout << "PI : " << result << std::endl;
+	return 0;
 }
