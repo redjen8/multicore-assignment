84f796b3036bad68c9244c781f61ba3915959713
redjen8 <seokwoo2525@gmail.com>
2022-06-08 23:40:26 +0900

    20220608 jsw Feat : prob2 실행 시간 측정

--- proj4/problem2/thrust_ex.cu
+++ proj4/problem2/thrust_ex.cu
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <cuda.h>
 #include <cmath>
-#include <thrust/fill.h>
+#include <chrono>
+#include <thrust/transform_reduce.h>
 #include <thrust/device_vector.h>
+#include <thrust/sequence.h>
 
 #define N 1000000000
 
@@ -27,6 +29,8 @@ struct convert_pi
 
 int main(void) {
 	double alpha = 1 / (double) N;
+
+	auto startTime = std::chrono::high_resolution_clock::now();
 	thrust::device_vector<int> data(N);
 	thrust::sequence(data.begin(), data.end());
 	// for (int i = 0; i<data.size(); i++) {
@@ -36,8 +40,10 @@ int main(void) {
 	thrust::plus<double> binary_op;
 	double result = thrust::transform_reduce(thrust::device, data.begin(), data.end(), unary_op, (double) 0.0, binary_op) * alpha;
 	gpuErrchk(cudaDeviceSynchronize());
-	// std::cout << alpha << std::endl;
+	auto endTime = std::chrono::high_resolution_clock::now();
+	auto timeDiff = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
 	std::cout.precision(24);
 	std::cout << "PI : " << result << std::endl;
+	std::cout << "Execution time : " << timeDiff.count() / 1000 << " ms" << std::endl;
 	return 0;
 }
